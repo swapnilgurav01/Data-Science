3885204e0a6acdef65967d54243c522f7dd03fe4
swapnilgurav01 <129293813+swapnilgurav01@users.noreply.github.com>
2023-04-14 17:34:14 +0530

    Add files via upload

---
+++ Rintroduction.R
@@ -0,0 +1,61 @@
+#variable
+a<- 2
+b<- 3
+c<- a+b
+h<-5
+g<-7
+j<- h+g
+j<- g+h
+print(c)
+c
+a+b
+a*b
+
+#numeric
+a<-7.5
+class(a)
+z<-9
+class(z)
+
+#character
+a<- c("sagar","ram","gaurav")
+b<- "saurabh"
+class(b)
+
+#logical
+a<-70>30
+class(a)
+a=70>30
+a<-50<20
+class(a)
+
+#R ARITHMETIC OPERATOR
+a+b
+a-b
+b*a
+b/a
+a-b/a
+2+3
+15-89
+90/4
+
+#exponential
+exp(2)
+exp(1*2)
+exp(8+7)
+class(exp)
+
+#square root (sqrt)
+sqrt(9)
+sqrt(100)
+sqrt(70)
+class(sqrt)
+
+#pi
+a+b*pi
+a
+b
+pi
+
+#R data structure
+ 
